7c699e5946e0c03ea39707bd99384797f8f4aa5d
Ashleni <sroy2005@outlook.com>
2022-09-23 14:04:14 -0400

    krewes implementation

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,3 +1,18 @@
+"""
+TEAM BLUE TIRAMISU: Shreya Roy, Eric Sohel, Faiyaz Rafee
+SoftDev
+09/23/2022
+
+DISCO:
+ - You have to import random before using it..
+QCC:
+
+OPS SUMMARY:
+We made a list of the keys and used random choice to choose a
+random period, and we used the length of that period's list
+to choose a random DEVO.
+
+"""
 import random
 krewes = {
            2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
@@ -5,10 +20,6 @@ krewes = {
            8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
          }
 mylist = [2,7, 8]
-def rand():
-    x = random.choice(mylist)
-    return x
-
 temp = krewes[random.choice(mylist)]
 val = random.randrange(1, len(temp), 1)
 
