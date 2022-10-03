419d743c657ba80ed82260ab97faad0fb13453cd
Ashleni <sroy2005@outlook.com>
2022-10-03 14:02:02 -0400

    teardown questions

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -5,19 +5,11 @@ K06 -- numbercruncher
 2022-09-29
 time spent: .9
 DISCO:
-- random.choices() can be used to weight choices
+- random.choices() can be used to weight choices 
 - 
--
+- 
 QCC:
-- 
 OPS SUMMARY:
-make_dict(filename): reads the file, splitting into unformatted strings 
-of each occupation and it's weight. Then a for loop splits each occupation
-    into its own key if it doesn't exist already, and sets the value to the
-    respective occupation's weight, and returns the dictionary.
-get_rand_weighted_krewe(nums): Checks if dictionary is empty.
-    If not, it picks one random occupation by choosing from the list of keys
-    and a list of their respective weights.
 '''
 
 import random
@@ -27,30 +19,33 @@ import random
 def make_dict(filename):
     dict = {} #makes dict to return
     file = open(filename)
-    file_str = file.read()
+    file_str = file.read()[21:]
     file.close()
+    #file_str = file_str.replace("\n","") #deals with newlines from texeditor
     #print(file_str)
     devo_info_str = file_str.split("\n") #gives unformatted str of each devo
-    # print(devo_info_str)
-    devo_info_str = devo_info_str[1:-1]
+    #print(devo_info_str)
     for devo in devo_info_str:
         if (devo[0:1] == '"'):
             infos = devo.rsplit(",",1)
+            #for n in devo:
         else:
             infos = devo.split(",") #devos info separated
-        # print(infos)
-        key = infos[0]
+        #print(infos)
+        key = infos[0] 
         if key not in dict:
-            dict[key] = None #make new key if pd isn't alr in dictionary
-        dict[key] = float(infos[1]) #pairs percent with job (key)
-    print(list(dict.keys()))
+            dict[key] = [] #make new key if pd isn't alr in dictionary
+        dict[key] = float(infos[1]) #appends tuple of dvo+ducky 
     return dict
+
+
+
 
 def get_rand_weighted_krewe(nums):
     if(len(nums) == 0):
         return ["empty dict"]
-    occupation = random.choices(list(nums.keys()), weights=list(nums.values()), k=1)
-    return occupation
+    key = random.choices(list(nums.keys())[1:-1], weights=list(nums.values())[1:-1], k=1)
+    return key
 
 
 def get_rand_krewe(nums):
---
+++ 07_teardown/app.py
@@ -0,0 +1,37 @@
+'''
+Team Keyboard: Eric Sohel, Shreya Roy
+SoftDev
+K07 -- teardown
+2022-10-03
+time spent: .3
+'''
+
+
+from flask import Flask
+
+app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
+
+@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+def hello_world():
+    print(__name__) # Q2: Where will this print to? Q3: What will it print?
+    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
+
+app.run()  # Q5: Where have you seen similar constructs in other languages?
+
+
+'''
+DISCO:
+QCC:
+0. Similar syntax seen from Java constructors.
+1. / usually indicates a directory, as it's the divider for different directories.
+   Probably means "app" is being directed to your home directory.
+2. Probably a separate file specified in whatever directory you routed "app" to.
+   Or maybe the terminal?
+3. It'll print whatever we will input for name. 
+4. No, as the string is being returned but hello_world() is never run.
+5. When you run a method in Java.
+...
+INVESTIGATIVE APPROACH:
+We used context clues alongside our prior knowledge to decipher what this code
+snippet might do when ran.
+'''
