42dcf55ddf84321fe2fe74d8f1bc8501b3846df3
Ashleni <sroy2005@outlook.com>
2022-11-06 14:12:03 -0500

    successfully learned sessions!

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,64 +1,47 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+# Ballers:: Shreya Roy, Jian Hong Li, Harry Zhu
+# SoftDev pd8
+# K19: session
+# 2022-11-7
+# time spent: 1.5 hours
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+from flask import session           #facilitate sessions!
+import os                           #facilitate key generation
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
+app.secret_key= os.urandom(32) #create random secret key
+#print(app.secret_key)
 
 
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not.
-TASK: Predict which...
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests.
-Process results.
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET']) #, methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    if request.method == "GET":
-        return render_template( 'login.html', request_method = "GET")
-    elif request.method == "POST":
-        username = request.form.get("username")
-        return render_template("response.html", username = username, request_method = "POST")
+    if 'username' in session:   #if already logged in
+        return render_template('response.html', username=session['username'], password=session['password'], request_method = "GET")
+    else:                       #if not logged in, send user to login page
+        return render_template('login.html')
 
-@app.route("/auth", methods=['GET', 'POST'])
+    
+@app.route("/auth", methods=['POST'])
 def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return "Waaa hooo HAAAH"  #response to a form submission
+    if request.method == 'POST':
+        session['username']= request.form['username']   #remember user in session
+        session['password'] = request.form['password']  #remember user in session
+        return render_template('response.html', username = request.form['username'], password = request.form['password'], request_method = 'POST') #response to a form submission
+    return "Waaa hooo HAAAH wowie howd u get here"  
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    if request.method == 'GET':
+        if session != {}:           #if no remembered user, just send to login page
+            session.pop('username') #un-remember username
+            session.pop('password') #un-remember password
+        return render_template('login.html') 
+    return "Waaa hooo HAAAH wowie howd u get here" 
 
 
 
