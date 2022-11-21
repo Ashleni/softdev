becd486810224713f1780e5bef7fed3fe36a0dd6
Ashleni <sroy2005@outlook.com>
2022-11-21 14:07:29 -0500

    api stuff

---
+++ 20_restapi/app.py
@@ -0,0 +1,12 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/", methods=['GET'])
+def home():
+    render_template("main.html")
