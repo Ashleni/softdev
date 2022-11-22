d55ba96fb30fea8200d8dd114e2a7080778e5824
Ashleni <sroy2005@outlook.com>
2022-11-22 14:02:17 -0500

    galaxies are cool

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,6 +1,6 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+import requests           #facilitate form submission
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -9,4 +9,18 @@ app = Flask(__name__)    #create Flask object
 
 @app.route("/", methods=['GET'])
 def home():
-    render_template("main.html")
+    key = open("key_nasa.txt")
+    #print(key.readline())
+    link = 'https://api.nasa.gov/planetary/apod?api_key=' + key.readline()
+    print(link)
+    r = requests.get(link[:-1])
+    dict = r.json()
+    url = dict['url']
+    explain = dict['explanation']
+    return render_template("main.html", url = url, explain = explain)
+    
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
