4a038c230bd38a652862a8e5333ae637d6adb249
Ashleni <sroy2005@outlook.com>
2023-04-25 15:26:17 -0400

    works yay

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -9,42 +9,44 @@ ctx.fillStyle = "blue"// YOUR CODE HERE
 var requestID;  // int global var for use with animation frames
 
 var clear = (e) => {
-  console.log("wiping canvas...")
+  //console.log("wiping canvas...")
   ctx.clearRect(0,0,c.clientWidth,c.clientHeight)
-  //console.log("ET VIOLA.  eh?")
 };
 
 var radius = 0;
 var growing = true;
 
 var drawDot = () => {
-  ctx.clearRect(0,0, c.width, c.height);
-  //var mouseX = event.offsetX;
-  //var mouseY = event.offsetY;
-  //console.log("mouseclick registered at ", mouseX, mouseY);
+  clear();
 
   ctx.beginPath();
-  ctx.arc(c.width/2, c.height/2, 50, 0, 2 * Math.PI);
+  ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
   
-  if (radius < 250){
-	  radius += 1;
-	  requestID = window.requestAnimationFrame(drawDot);
+  if (radius > 250 && growing){
+	  growing = false;
+  }
+  else if (radius-1 < 0 && !growing) {
 	  growing = true;
-	  
   }
   
-  radius -= 1;
-  requestID = window.requestAnimationFrame(drawDot);
-  growing = false;
+  if(growing){
+	  radius += 1;
+  }
+  else {
+	  radius -= 1;
+  }
+  
   window.cancelAnimationFrame(requestID);
+  requestID = window.requestAnimationFrame(drawDot);
 };
 
 //var stopIt = function() {
 var stopIt = () => {
 	console.log("stopIt invoked...")
 	console.log( requestID );
+	window.cancelAnimationFrame(requestID);
 	
 	
 };
